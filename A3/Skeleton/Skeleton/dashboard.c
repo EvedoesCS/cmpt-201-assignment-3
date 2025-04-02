52228a279f4fe201947c2456a5c2ac6cbaae1e82
hele-L <lyl9@mymacewan.ca>
2025-04-01 23:17:28 -0600

    Comment out some stuff

--- A3/Skeleton/Skeleton/dashboard.c
+++ A3/Skeleton/Skeleton/dashboard.c
@@ -17,19 +17,20 @@
 //You may include other original headers as you see fit
 
 #include <stdio.h>
-#include <stdlib.h> //use for atoi() 
+#include <stdlib.h> //use for atoi()
+#include <string.h> //used for strlen 
 #include "DB.h"
-//#include "import.c" //TOBEREMOVED - put import.c in db.c
-#include "export.c"//TOBEREMOVED - move it to db.c
-#include "editTableEntry.c"//TOBEREMOVED - move to db.c
+//#include "export.c"//TOBEREMOVED - move it to db.c
+//#include "editTableEntry.c"//TOBEREMOVED - move to db.c
 
 
 #define INPUT_LEN 10 //maxlen when selecting menu options
-#define FILENAME_LEN 100 //maxlen for filename
+#define FILENAME_LEN 512 //maxlen for filename
 #define MAX_LEN 256//maxlen for any buffers 
 
 
 /*********************************************************************
+Author: Helen Ly
 Purpose: displaying main menu 
 Arguments: void
 Returns: void
@@ -48,55 +49,58 @@ void displayMenuOptions(){
 }
 
 /*********************************************************************
+Author: Helen Ly
 Purpose: displaying menu for memberName options and prompt user input to be used for 
          option 3 in main menu - count entries
 Arguments: void
 Returns: memberName string based on user input
 **********************************************************************/
-char* memberNameforCountEntries(){
-    //string array of memberName options
-    char* options[6]= {"Table Type", "Surface Material", "Structural Material","Neighborhood ID","Neighborhood Name", "Ward"};
-    printf("Select member name that need to be counted: \n");
-    //loop through the array to display memberName menu
-    for (int i =0; i<6; i++){
-        printf("%d. %s\n", i+1, options[i]);
-    }
+//char* memberNameforCountEntries(){
+//     //string array of memberName options
+//     char* options[6]= {"Table Type", "Surface Material", "Structural Material","Neighborhood ID","Neighborhood Name", "Ward"};
+//     printf("Select member name that need to be counted: \n");
+//     //loop through the array to display memberName menu
+//     for (int i =0; i<6; i++){
+//         printf("%d. %s\n", i+1, options[i]);
+//     }
     
-    char memberName[INPUT_LEN];
-    int select = 0;
+//     char memberName[INPUT_LEN];
+//     int select = 0;
 
-    while (select<1 || select >6){
-        //when input successfully, the input will be cast to be an int with under select variable
-        if (fgets(memberName, INPUT_LEN, stdin) != NULL){
-            select = atoi(memberName);
-        }
-        //when input is NULL, display error message and prompt user input again
-        else{
-            fprintf(stderr, "Please only choose one number from 1 to 7.");
-        }
+//     while (select<1 || select >6){
+//         //when input successfully, the input will be cast to be an int with under select variable
+//         if (fgets(memberName, INPUT_LEN, stdin) != NULL){
+//             select = atoi(memberName);
+//         }
+//         //when input is NULL, display error message and prompt user input again
+//         else{
+//             fprintf(stderr, "Please only choose one number from 1 to 7.\n");
+//         }
 
-        //different return strings based on user input
-        if (select >= 1 || select <= 6){
-            return options[select-1];
-        }
+//         //different return strings based on user input
+//         if (select >= 1 && select <= 6){
+//             return (options[select-1]);
+//         }
        
-        //when input is not within the menu option, print error message and ask for input again
-        else{
-            printf("Selection was invalid, please select a number from 1 to 7.\n");
-            fgets(memberName, INPUT_LEN, stdin);                
-        }
-    return NULL;       
-}
+//         //when input is not within the menu option, print error message and ask for input again
+//         else{
+//             printf("Selection was invalid, please select a number from 1 to 7.\n");
+//             //fgets(memberName, INPUT_LEN, stdin);  
+//             //select = atoi(memberName);
+              
+//         }
+//     return NULL;       
+// }
 
 /*------------------------------------------------------------------------
+Author: Helen Ly
 Purpose: displaying menu for memberName options and prompt user input
          to be used in option 4 of main menu - sort member
 Arguments: void
 Returns: memberName string based on user input
 ------------------------------------------------------------------------*/
 
-char* memberNameforSortbyMember()
-{
+char* memberNameforSortbyMember(){
     char* options[5] = {"Table Type", "Surface Material", "Structural Material", "Neighborhood Name", "Ward"};
 
     for (int i = 0; i < 5; i++) {
@@ -121,6 +125,7 @@ char* memberNameforSortbyMember()
 
 
 /*********************************************************************
+Author: Helen Ly
 Purpose: get user input to get memberName
 Arguments: void
 Returns: string of valid memberName or NULL
@@ -145,17 +150,19 @@ char* memberNameforEditEntryOptions(){
     
 }
 
+/*
 //function prototypes
 void countEntriesOptions(); 
 void sortByOptions();
 void editEntryOptions();
+*/
 
 
 // use as condition to Prevent operations until the database is imported 
 int DatabaseImported = 0; //Database hasn't been imported
 
 
-int main(){
+int main() {
 
     int cmd = 0;
 
@@ -164,9 +171,15 @@ int main(){
     while (cmd != 7){ //the program keep running until the user choose option 7
         displayMenuOptions();
         if (fgets(input, INPUT_LEN, stdin) != NULL){
-            while (strlen(input) != 1){
+            for (int i=0; i<INPUT_LEN; i++){
+                if (input[i] == '\n'){
+                    input[i] = '\0';
+                    break;
+                }
+            }
+            while (strlen(input) != 1|| input[0]<'1'|| input[0]>'7'){
                 fprintf(stderr, "Please only choose one number from 1 to 7.");
-                //fgets(input, INPUT_LEN, stdin);
+                continue;
             }
             cmd = atoi(input);
         }
@@ -184,20 +197,28 @@ int main(){
         if (cmd == 1){
 
             char filename[FILENAME_LEN];
-            printf("Enter the filename to be imported: ");
-            if (fgets(filename, FILENAME_LEN, stdin) != NULL){
+            printf("Enter the filename to be imported: \n");
+            if (fgets(filename, sizeof(filename), stdin) != NULL){
+                printf("Did it go here?");
+                filename[strcspn(filename, "\n")] = '\0';
+/*
+                for (int i=0; i<FILENAME_LEN; i++){
+                    if (filename[i] == '\n'){
+                        filename[i] = '\0';
+                        break;
+                    }
+                }*/
+                printf("or here?");
                 importDB(filename);
-                //prompt_file_path(filename);
-                //load_from_file(filename);
                 DatabaseImported = 1; //Update when database is imported sucessfully
-                printf("File %s has been imported", filename);
+                printf("File %s has been imported.\n", filename);
             }
             else {
                 exit(0);
             }
         }
         
-        else if (cmd == 2){
+        /*else if (cmd == 2){
             char filename[FILENAME_LEN];
             printf("Enter the filename to be imported: ");
             if (fgets(filename, FILENAME_LEN, stdin) != NULL){
@@ -209,7 +230,9 @@ int main(){
                 exit(0);
             }
         }
+        */
 
+        /*
         else if(cmd == 3){
             countEntriesOptions();
         }
@@ -226,17 +249,21 @@ int main(){
         else if (cmd==6){
             sortByOptions();
         }
+        */
     }
     return 0;
+
 }
 
+
 /*********************************************************************
+Author: Helen Ly
 Purpose: displaying menu for memberName options, get user input for searchValue
          and call countEntries() function using memberName and seachValue
 Arguments: void
 Returns: void
 **********************************************************************/
-void countEntriesOptions(){
+/*void countEntriesOptions(){
 
     char* memberName = memberNameforCountEntries();
 
@@ -257,16 +284,17 @@ void countEntriesOptions(){
     printf("There are %d entries of %s.\n", count, searchValue);
 
 }
-
+*/
 
 
 
 /*********************************************************************
+Author: Helen Ly
 Purpose: get user input for report type options, then generate requested report
 Arguments: void
 Returns: void
 **********************************************************************/
-void sortByOptions(){
+/*void sortByOptions(){
 
     //display menu by looping through the report type options
     char* options[2] = {"Report By Ward", "Report By Neighborhood ID"};
@@ -293,14 +321,16 @@ void sortByOptions(){
     else{
         printf ("Selection is invalid. Only choose 1 or 2.\n");
     }
-}
+}*/
 
 /*********************************************************************
+Author: Helen Ly
 Purpose: get user input for tableID, memberName and newValue as input
          argument to call editTableEntry() function
 Arguments: void
 Returns: void
 **********************************************************************/
+/*
 void editEntryOptions(){
 
     char tableID[INPUT_LEN];
@@ -332,3 +362,4 @@ void editEntryOptions(){
     editTableEntry(atoi(tableID), memberName, newValue);
 
 }
+    */
